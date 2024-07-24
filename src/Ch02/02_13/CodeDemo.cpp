36457495a3677e9a3e6049ef841ecad83480fd17
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 16:30:44 +0000

    average challenge

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -6,9 +6,13 @@
 
 int main(){
     int nums[5] = {1,23,32,24,337};
-    float result; 
+    float result = 0; 
 
     // Write your code here
+    for (int i = 0; i < 5; i++){
+        result += nums[i];
+    }
+    result /= 5.0;
     
     std::cout << "The average is " << result << std::endl;
     
