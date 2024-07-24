11e45546d1b598b5541d6a1c9b634b46517d53e5
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 13:30:06 +0000

    arrays

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -3,9 +3,16 @@
 // Arrays, by Eduardo Corpeño 
 
 #include <iostream>
+using namespace std;
 
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    const size_t AGE_LENGTH = 4;
+    int age[AGE_LENGTH];
+    for (int i = 0; i < AGE_LENGTH; i++){
+        age[i] = i*2;
+        cout << "Age[" << i << "] = " << age[i] << endl;
+    }
+
+    cout << endl << endl;
     return (0);
 }
