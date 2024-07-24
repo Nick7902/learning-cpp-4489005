0ca1ef61ca094ef39f8cab6a01b89c184c3996aa
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 17:42:16 +0000

    Vectors and objects

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -6,9 +6,20 @@
 #include <iostream>
 #include <string>
 #include "cow.h"
+using namespace std;
 
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    vector<cow> cattle;
+    cattle.push_back(cow("Betty", 6, cow_purpose::meat));
+    cattle.push_back(cow("Bernard", 4, cow_purpose::dairy));
+    cattle.push_back(cow("Ben", 5, cow_purpose::hide));
+    cattle.push_back(cow("Joe", 8, cow_purpose::pet));
+
+    cout << "The first cow is " << cattle.begin()->get_name() << endl;
+    cout << "The second cow is " << cattle[1].get_name() << endl;
+    cout << "The second to last cow is " << prev(cattle.end(), 2)->get_name() << endl;
+    cout << "The last cow is " << (cattle.end() - 1)->get_name() << endl;
+
+    cout << endl << endl;
     return (0);
 }
