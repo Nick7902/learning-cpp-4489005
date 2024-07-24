6a2b92829b87c4c79c6a359aaa4480a40b2ba2bd
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 19:33:41 +0000

    GPA challenge

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -5,33 +5,60 @@
 #include <iostream>
 #include <vector>
 #include "records.h"
+using namespace std;
 
 int main(){
     float GPA = 0.0f;
     int id;
     
-    std::vector<Student> students = {Student(1, "George P. Burdell"),
+    vector<Student> students = {Student(1, "George P. Burdell"),
                                     Student(2, "Nancy Rhodes")};
 
-    std::vector<Course> courses = {Course(1, "Algebra", 5),
+    vector<Course> courses = {Course(1, "Algebra", 5),
                                 Course(2, "Physics", 4),
                                 Course(3, "English", 3),
                                 Course(4, "Economics", 4)};
 
-    std::vector<Grade> grades = {Grade(1, 1, 'B'), Grade(1, 2, 'A'), Grade(1, 3, 'C'),
+    vector<Grade> grades = {Grade(1, 1, 'B'), Grade(1, 2, 'A'), Grade(1, 3, 'C'),
                                 Grade(2, 1, 'A'), Grade(2, 2, 'A'), Grade(2, 4, 'B')};
 
-    std::cout << "Enter a student ID: " << std::flush;
-    std::cin >> id;
+    cout << "Enter a student ID: " << flush;
+    cin >> id;
 
     // Calculate the GPA for the selected student.
     // Write your code here
+    float grade_weight = 0.0f;
+    int num_credits = 0;
 
-    std::string student_str;
+    for (int i = 0; i < grades.size(); i++){
+
+        if (grades[i].get_student_id() == id){
+            float grade_num;
+            switch(grades[i].get_grade()){
+                case 'A': grade_num = 4.0f;
+                    break;
+                case 'B': grade_num = 3.0f;
+                    break;
+                case 'C': grade_num = 2.0f;
+                    break;
+                case 'D': grade_num = 1.0f;
+                    break;
+                default: grade_num = 0.0f;
+                    break;
+            }
+
+            num_credits += courses[grades[i].get_course_id() - 1].get_credits();
+            grade_weight += grade_num *  courses[grades[i].get_course_id() - 1].get_credits();
+        }
+    }
+    float gpa = grade_weight / num_credits;
+    cout << "The GPA is: " << gpa << endl;
+
+    string student_str;
     student_str = students[0].get_name(); // Change this to the selected student's name
 
-    std::cout << "The GPA for " << student_str << " is " << GPA << std::endl;
+    cout << "The GPA for " << student_str << " is " << GPA << endl;
     
-    std::cout << std::endl << std::endl;
+    cout << endl << endl;
     return (0);
 }
