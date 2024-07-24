4244266bc28893ab9902421245755572654285dd
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 13:14:48 +0000

    Auto inference

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,9 +4,19 @@
 
 #include <iostream>
 #include <typeinfo>
+using namespace std;
 
 int main(){
+    // Auto inference, determines the type based on the value given
     
-    std::cout << std::endl << std::endl;
+    auto a = 5;
+    auto b = 12345678901;
+    auto c = true;
+
+    cout << "The type of a is " << typeid(a).name() << endl;
+    cout << "The type of b is " << typeid(b).name() << endl;
+    cout << "The type of c is " << typeid(c).name() << endl;
+
+    cout << endl << endl;
     return (0);
 }
