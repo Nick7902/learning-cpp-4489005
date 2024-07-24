8bfd1f05717c9ce78ef09fa721a6dd9a6b60f6f9
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 17:35:47 +0000

    vectors

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -4,9 +4,22 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+using namespace std;
 
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    vector<int> primes;
+
+    primes.push_back(2);
+    primes.push_back(5);
+    primes.push_back(9);
+    primes.push_back(7);
+
+    cout << "The vector primes has " << primes.size() << " elements." << endl;
+    cout << "The value at index 2 is: " << primes[2] << endl;
+    primes[2] = 10;
+    cout << "The value at index 2 is: " << primes[2] << endl;
+
+    cout << endl << endl;
     return (0);
 }
