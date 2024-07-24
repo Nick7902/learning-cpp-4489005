542d3a39a445765b3b40932f670be3e64ffa08af
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 12:30:42 +0000

    Hello world

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    cout << endl;
+
+    return 0;
+}
