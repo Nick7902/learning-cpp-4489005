a18701eeab44d0013a2e625ebca4fd2f8f5e5404
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 17:19:40 +0000

    Header and implementation files

--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -4,39 +4,14 @@
 
 #include <iostream>
 #include <string>
-
-enum class cow_purpose {dairy, meat, hide, pet};
-
-class cow{
-public:
-    cow(std::string name_i, int age_i, cow_purpose purpose_i){
-        name = name_i;
-        age = age_i;
-        purpose = purpose_i;
-    }
-    std::string get_name() const{
-        return name;
-    }
-    int get_age() const{
-        return age;
-    }
-    cow_purpose get_purpose() const{
-        return purpose;
-    }
-    void set_age(int new_age){
-        age = new_age;
-    }
-private:
-    std::string name;
-    int age;
-    cow_purpose purpose;
-};
+#include "cow.h"
+using namespace std;
 
 int main(){
     cow my_cow("Hildy", 7, cow_purpose::pet);
-    std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
-    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+    cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << endl;
+    cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << endl;
     
-    std::cout << std::endl << std::endl;
+    cout << endl << endl;
     return (0);
 }
---
+++ src/Ch03/03_05b/cow.h
@@ -0,0 +1,22 @@
+// Include guard
+#ifndef COW_H
+#define COW_H
+#include <string>
+using namespace std;
+
+enum class cow_purpose {dairy, meat, hide, pet};
+
+class cow{
+public:
+    cow(string name_i, int age_i, cow_purpose purpose_i);
+    string get_name() const;
+    int get_age() const;
+    cow_purpose get_purpose() const;
+    void set_age(int new_age);
+private:
+    string name;
+    int age;
+    cow_purpose purpose;
+};
+
+#endif // COW_H
