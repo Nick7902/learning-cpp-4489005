a4eab2903ba6167a0ab4c6a1de2fc385999b28c6
Nick7902 <63563703+Nick7902@users.noreply.github.com>
2024-07-24 13:21:42 +0000

    Preprocessor directives

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -2,10 +2,27 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
+// Preprocessor directives
 #include <iostream>
+#include <string>
+#include <cstdint> // stdint from C converted for C++
+using namespace std;
+
+//Constant
+#define CAPACITY 5000
+#define DEBUG // if this line is commented the dubug section will not be included
 
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+
+#ifdef DEBUG
+    cout << "[About to perform the addition]" << endl;
+#endif
+
+    large += small;
+    cout << "The large integer is " << large << endl;
+
+    cout << endl << endl;
     return (0);
 }
